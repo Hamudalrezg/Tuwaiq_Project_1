baf8dfc90ae7c9afa6825b21228e63482d847e25
[hamudalrezg] <[hamud.alrzg.eam.js@tuwaiq.edu.sa]>
2021-10-26 16:53:57 +0300

    proj

--- js/js01.js
+++ js/js01.js
@@ -4,42 +4,50 @@
 //For SignUp: Save in local storage   localStorage.set('users',{email, password})
 // let log = [{email, password}, {email, password}]
 
-//$('document').ready(()=>{
-   // chatHistory.forEach(chatHistory =>console.log(chat); 
-        
-    //);
-//})
+$('document').ready(() => {
+    const history = JSON.parse(window.localStorage.getItem("chat"))
+    if (history !== null) {
+        history.forEach(function (tex, index) {
+            if (index % 2 === 0) {
+                $('.chat').append(`<li class=${"txt" + 1}>${tex}</li>`)
+            } else {
+                $('.chat').append(`<li class=${"txt" + 2}>${tex}</li>`)
+            }
+        });
+    }
+})
 
 
 
 function goToPage3() {
-    console.log("hello");
+
     window.location.href = "../html/ht03.html";
 }
 
 function goToPage03() {
-    console.log("hello");
+
     window.location.href = "../html/ht03.html";
 }
 
 function goToPage003() {
-    console.log("hello");
+
     window.location.href = "../html/ht03.html";
 }
 $('#addComment').click(() => {
     const comment = $('#floatingTextarea2').val();
-    
+
     const chatHistory = JSON.parse(window.localStorage.getItem("chat"));
     if (chatHistory != null) {
         chatHistory.push(comment)
         window.localStorage.setItem("chat", JSON.stringify(chatHistory));
     } else {
-        window.localStorage.setItem("chat", JSON.stringify([comment]))        
+        window.localStorage.setItem("chat", JSON.stringify([comment]))
     }
     chatHistory.push(comment)
-if (chatHistory.length % 2 === 0) {
-    $('.chat').append(`<li class=${"txt" + 1}>${chatHistory[chatHistory.length-1]}</li>`)
-} else {
-    $('.chat').append(`<li class=${"txt" + 2}>${chatHistory[chatHistory.length-1]}</li>`)
-}
+    if (chatHistory.length % 2 === 0) {
+        $('.chat').append(`<li class=${"txt" + 1}>${chatHistory[chatHistory.length-1]}</li>`)
+    } else {
+        $('.chat').append(`<li class=${"txt" + 2}>${chatHistory[chatHistory.length-1]}</li>`)
+    }
+    $('#floatingTextarea2').val('');
 })
